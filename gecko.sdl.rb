da5f5b6ebeb2782ad2d8be3cb28c08fb0dfd36ba
TriFox Dev Team <ghostsec2424@gmail.com>
2025-05-10 14:57:23 -0600

    Create gecko.sdl.rb

---
+++ gecko.sdl.rb
@@ -0,0 +1,13 @@
+s_
+
+[3.5] | {
+       on.program user(det) call rep('apt') on funcTerm exclude(db-md) 
+       read apt replace(as_term) = "gecko" 
+       for.user call dependant pass_form on:exec 
+}
+
+[3.5] | {
+       apt_func read *as (prev_rollback) continue func.exec_comp
+       const:pool(by_active_con) read *as pm(func.exec:still) replacec.det(call) apt = gecko_(convert)
+}
+
